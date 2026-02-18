673e618c392a847921d8e8327947a5dafb9f332e
Catalin Bozan <catabozan@protonmail.com>
2026-02-18 15:05:31 +0100

    fix: remove api keys oops

--- playground/api-wrappers/SpotifyApi.py
+++ playground/api-wrappers/SpotifyApi.py
@@ -1,7 +1,4 @@
 from . import ApiInterface
 
-SPOTIFY_CLIENT_ID = "7871501fd88b41cba20c6fc5ce5b23c4"
-SPOTIFY_CLIENT_SECRET = "7a3f5de27bbd4ab3b54ca1102e017f44"
-
 class SpotifyApi(ApiInterface):
     pass
