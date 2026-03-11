5188ce7e959b99f9559f9bfc5b57913ebb25b149
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2026-03-11 16:10:37 +0100

    Use dotenv for settings

--- api/meow_sync/settings.py
+++ api/meow_sync/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6i6x@*#$nxy7f_k(vwo3t28php6@p(o&7ou95))rjpuz)ukm2_'
+SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-6i6x@*#$nxy7f_k(vwo3t28php6@p(o&7ou95))rjpuz)ukm2_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG', 'True').lower() == 'true' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST', 'localhost:8000')
+]
 
 
 # Application definition
