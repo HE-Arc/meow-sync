526a19da496a7e9e9bb14d37919feba41122c916
Catalin Bozan <catabozan@protonmail.com>
2026-03-11 15:53:36 +0100

    feat(backend): custom user model

--- api/meow_sync_app/admin.py
+++ api/meow_sync_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin  # noqa: F401
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Auth Tokens", {
+            "fields": ("spotify_token", "youtube_token"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Auth Tokens", {
+            "fields": ("spotify_token", "youtube_token"),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- api/meow_sync_app/models.py
+++ api/meow_sync_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models  # noqa: F401
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass
+    spotify_token = models.TextField(blank=True)
+    youtube_token = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
