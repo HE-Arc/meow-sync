6282776171771d73ea179e963bd3a777fc866034
Catalin Bozan <catabozan@protonmail.com>
2026-03-11 15:19:30 +0100

    feat(frontend): add login with spotify and youtube buttons

---
+++ frontend/src/components/LoginYoutubeBtn.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const backend_url: string = import.meta.env.VUE_APP_API_URL ?? ""
+</script>
+
+<template>
+  <div>
+    <a :href="backend_url + '/login/youtube'">Log In with YouTube</a>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/views/LoginVue.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import LoginSpotifyBtn from '../components/LoginSpotifyBtn.vue';
+import LoginYoutubeBtn from '../components/LoginYoutubeBtn.vue';
+</script>
+
+<template>
+  <div class="login">
+    <h1>This is a login page</h1>
+    <LoginSpotifyBtn/>
+    <LoginYoutubeBtn/>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
