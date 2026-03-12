ca79a56b488daee4a6fdc6d16e7b49c3f7142474
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2026-03-12 17:33:22 +0100

    feat: Configure @nuxt/ui and tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+import ui from "@nuxt/ui/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueDevTools()],
+	plugins: [vue(), ui(), vueDevTools()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
