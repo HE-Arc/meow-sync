0915e176dba5eccbe00265b34999080557d23b5f
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2026-03-25 15:44:00 +0100

    feat(frontend): Add 404 and 403 error pages and add better way to check for authentification

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+const props = defineProps<{
+  errorCode: number;
+  errorMessage: string;
+}>();
+</script>
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <img
+      :src="`https://http.cat/${props.errorCode}.jpg`"
+      :alt="`Error ${props.errorCode} cat`"
+      class="h-150"
+    />
+
+    <UButton class="mt-4" to="/">Go to Home</UButton>
+  </div>
+</template>
