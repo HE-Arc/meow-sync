7d34e01b2e529d2ae803588463ff4a987c5dd6d2
Catalin Bozan <catabozan@protonmail.com>
2026-03-25 15:01:21 +0100

    fix: add REDIRECT_URI in .env

--- api/meow_sync_app/music_providers_api/SpotifyApi.py
+++ api/meow_sync_app/music_providers_api/SpotifyApi.py
@@ -7,11 +7,11 @@ from .ApiInterface import *
 class SpotifyApi(ApiInterface):
     CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
     CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+    REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
     AUTH_URL = "https://accounts.spotify.com/authorize"
     TOKEN_URL = "https://accounts.spotify.com/api/token"
     API_BASE_URL = "https://api.spotify.com/v1"
-    REDIRECT_URI = "http://127.0.0.1:8000/spotify/callback"
 
     @classmethod
     def login_url(self, state: str) -> str:
