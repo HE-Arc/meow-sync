675f251968689b0baa5c8bdb6c953fd8581263ff
Yohan Zbinden <46626633+yoyozbi@users.noreply.github.com>
2026-03-25 15:49:33 +0100

    feat(frontend): integrate http.cat for router errors and fix scrollbar (#43)

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+
+const props = defineProps<{
+  errorCode: number;
+  errorMessage: string;
+}>();
+</script>
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <!-- biome-ignore lint/a11y/useAltText: We have an alt that is binded to the error code -->
+    <img
+      :src="`https://http.cat/${props.errorCode}.jpg`"
+      :alt="`Error ${props.errorCode} cat`"
+      class="h-150"
+    />
+
+    <UButton class="mt-4" to="/">Go to Home</UButton>
+  </div>
+</template>
